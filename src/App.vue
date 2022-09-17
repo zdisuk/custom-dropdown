717e90fa289bf464211de1ce8634e2704eb96649
zdisuk oleksandr <zdisuk.oleksandr@gmail.com>
2022-09-17 14:26:12 +0300

    add basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-dropdown></the-dropdown>
+  <the-dropdown :users="users"></the-dropdown>
 </template>
 
 <script>
@@ -8,10 +8,54 @@ export default {
   components: {
     TheDropdown
   },
+  data(){
+    return {
+      users: [
+        {id: 0, name: "Alex", isActive: true,},
+        {id: 1, name: "Bob", isActive: false,},
+        {id: 2, name: "Jack", isActive: false,},
+        {id: 3, name: "Kate", isActive: false,},
+        {id: 4, name: "Joseph", isActive: false,},
+        {id: 5, name: "Tom", isActive: false,},
+        {id: 6, name: "Montana", isActive: false,},
+        {id: 7, name: "Tony", isActive: false,},
+        {id: 8, name: "Pitter", isActive: false,},
+        {id: 9, name: "Caroline", isActive: false,},
+        {id: 10, name: "Andrew", isActive: false,},
+        {id: 11, name: "Pall", isActive: false,},
+        {id: 12, name: "Brad", isActive: false,},
+        {id: 13, name: "Carl", isActive: false,},
+        {id: 14, name: "Luis", isActive: false,},
+        {id: 15, name: "Fenix", isActive: false,},
+        {id: 16, name: "Julie", isActive: false,},
+        {id: 17, name: "Rihana", isActive: false,},
+        {id: 18, name: "Rocky", isActive: false,},
+        {id: 19, name: "Rick", isActive: false,},
+        {id: 20, name: "Leonardo", isActive: false,},
+        {id: 21, name: "Angelina", isActive: false,},
+        {id: 22, name: "Arnold", isActive: false,},
+        {id: 23, name: "Fury", isActive: false,},
+        {id: 24, name: "German", isActive: false,},
+      ]
+    }
+  }
 
 }
 </script>
 
 <style>
-
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: sans-serif;
+  font-style: normal;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
--- src/components/dropdown/TheDropdown.vue
+++ src/components/dropdown/TheDropdown.vue
@@ -1,13 +1,119 @@
 <template>
-  
+  <div class="wrapper">
+    <div class="input-bar">
+      <input
+        class="input-bar__search"
+        placeholder="Click to filter"
+        type="text"
+      >
+      <div class="input-bar__button">
+        <div class="input-bar__arrow"></div>
+      </div>
+    </div>
+    <div class="list">
+      <div class="list__title">Simple filter</div>
+      <div class="list__options">
+        <p
+          v-for="user in users"
+          :key="user.id"
+          class="list__option"
+        >{{ user.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    users: {
+      type: Object,
+      required: true
+    }
+  }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.wrapper {
+  width: 150px;
+  min-height: 300px;
+  max-height: fit-content;
+}
+.input-bar {
+  display: flex;
+  cursor: pointer;
+  margin-bottom: 10px;
+		&__search {
+      color: #000;
+      height: 40px;
+      width: 80%;
+      padding: 0 5px;
+      text-align: center;
+      font-size: 1rem;
+      outline: none;
+      border: 1px solid gray;
+      border-right: none;
+      border-radius: 4px;
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+		}
 
+		&__button {
+      width: 20%;
+      height: 40px;
+      border: 1px solid gray;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-left: none;
+      border-radius: 4px;
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+		}
+
+		&__arrow {
+      border: solid black;
+      border-width: 0 2px 2px 0;
+      display: inline-block;
+      transform: rotate(45deg);
+      padding: 3px;
+      transition: all 0.3s ease-out;
+		}
+}
+.list {
+  width: 150%;
+  height: fit-content;
+  border: 1px solid gray;
+  border-radius: 4px;
+		&__title {
+      position: relative;
+      top: 0;
+      padding: 15px 10px;
+      text-align: center;
+      border-bottom: 1.5px solid gray;
+      color: darkgray;
+      font-weight: bold;
+		}
+
+		&__options {
+      overflow: scroll;
+      height: fit-content;
+      max-height: 250px;
+		}
+
+		&__option {
+      color: #333;
+      padding: 5px 10px;
+      display: flex;
+      justify-content: center;
+      font-size: 1rem;
+        &:hover{
+          background-color: darkgray;
+          cursor: pointer;
+          color: black;
+        }
+		}
+}
 </style>
